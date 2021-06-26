8b81b65dfba052602e5a39bcf458c30a2b66482c
My Computer <mycomputer44985@gmail.com>
2021-06-27 00:01:48 +0600

    First commit korlam

---
+++ New Text Document.bat
@@ -0,0 +1,9 @@
+@echo off
+
+git init
+git remote add origin https://github.com/ShubratoDn/data-pachar.git
+git remote -v
+git add .
+git commit -m "First commit korlam"
+git push origin master
+pause
